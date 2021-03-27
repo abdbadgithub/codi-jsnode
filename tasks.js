ebbb6cb1defb7cfd2963260edbfe5fe190983c6c
abd <62624790+abdbadgithub@users.noreply.github.com>
2021-03-27 09:51:38 +0200

    added jsdoc for help

--- tasks.js
+++ tasks.js
@@ -14,6 +14,7 @@ function startApp(name) {
   process.stdin.on("data", onDataReceived);
   console.log(`Welcome to ${name}'s application!`);
   console.log("--------------------");
+  console.log("type help to know the commands");
 }
 
 /**
